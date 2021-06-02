caf9262b205b85cff0e99f46269a6db2325c9f43
ErikDRen <ren.erik.19@gmail.com>
2021-06-02 14:41:42 +0200

    :tada:

--- test/main.go
+++ test/main.go
@@ -1,94 +1,3 @@
-// package main
-
-// import (
-// 	"database/sql"
-// 	"fmt"
-// 	"html/template"
-// 	"log"
-// 	"net/http"
-// 	"strconv"
-// )
-
-// type infoData struct {
-// 	id       int
-// 	username string
-// 	newPost    string
-// 	password string
-// 	newPost  string
-// }
-
-// func mainHandle(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Println("HOM")
-// 	data := "test"
-// 	tpl := template.Must(template.ParseFiles("assets/index.html"))
-// 	err := tpl.Execute(w, data)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
-
-// func main() {
-// 	fmt.Println("TTTrrrrooooppppp ccccooooollllll ssaaaaaa mmmmmarccchhhhheeee :)))))))")
-// 	fs := http.FileServer(http.Dir("assets"))
-// 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
-// 	http.Handle("/", fs)
-// 	http.HandleFunc("/main", mainHandle)
-// 	http.HandleFunc("/post", PostHandle)
-// 	http.ListenAndServe(":6060", nil)
-// }
-
-// func database(username string, newPost string) {
-
-// 	database, _ :=
-// 		sql.Open("sqlite3", "data.db")
-// 	statement, _ :=
-// 		database.Prepare("CREATE TABLE IF NOT EXISTS post (id INTEGER PRIMARY KEY, username TEXT, newPost TEXT)")
-// 	statement.Exec()
-// 	statement, _ =
-// 		database.Prepare("INSERT INTO post (username, newPost) VALUES (?, ?)")
-// 	statement.Exec(username, newPost)
-// 	rows, _ :=
-// 		database.Query("SELECT id, username, newPost FROM post")
-// 	var id int
-// 	for rows.Next() {
-// 		rows.Scan(&id, &username, &newPost)
-// 		fmt.Println(strconv.Itoa(id) + ": " + username + " " + newPost)
-// 	}
-
-// }
-
-// func PostHandle(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Println("post")
-
-// 	// userName := r.FormValue("username")
-// 	// userName := "Chocolat"
-// 	// newPost := r.FormValue("newPost")
-// 	var postTab []string
-
-// 	idata := infoData{
-// 		username: "doof",
-// 		newPost:  "Salut tout le monde.",
-// 	}
-// 	tpl := template.Must(template.ParseFiles("assets/test.html"))
-
-// 	// if userName != "" && newPost != "" {
-// 	fmt.Println("tu rentres dedans wsh")
-// 	database(idata.username, idata.newPost)
-// 	http.Redirect(w, r, "/main", http.StatusSeeOther)
-// 	// }
-
-// 	postTab = append(postTab, idata.username)
-// 	postTab = append(postTab, idata.newPost)
-// 	println(postTab[1])
-
-// 	data := "test"
-
-// 	err := tpl.Execute(w, data)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
-
 package main
 
 import (
@@ -102,12 +11,12 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-type databaseInfo struct {
+type postInfo struct {
 	idx     int
 	pseudo  string
 	message string
 }
-type comment struct {
+type postMessage struct {
 	id      int
 	image   string
 	pseudo  string
@@ -116,13 +25,14 @@ type comment struct {
 }
 
 type oui struct {
-	Postee []comment
+	Poste   []postMessage
+	Comment []postMessage
 }
 
-func database(username string, newPost string) {
+func databasePost(username string, newPost string) {
 
 	database, _ :=
-		sql.Open("sqlite3", "test.db")
+		sql.Open("sqlite3", "data.db")
 	statement, _ :=
 		database.Prepare("CREATE TABLE IF NOT EXISTS post (id INTEGER PRIMARY KEY, username TEXT, newPost TEXT)")
 	statement.Exec()
@@ -130,7 +40,6 @@ func database(username string, newPost string) {
 		database.Prepare("INSERT INTO post (username, newPost) VALUES (?, ?)")
 	fmt.Println("ici")
 	statement.Exec(username, newPost)
-	fmt.Println("G TROUVER VOUS ETES NUL")
 	rows, _ :=
 		database.Query("SELECT id, username, newPost FROM post")
 	var id int
@@ -138,29 +47,95 @@ func database(username string, newPost string) {
 	for rows.Next() {
 		rows.Scan(&id, &username, &newPost)
 		test = append(test, strconv.Itoa(id)+": "+username+" "+newPost+"\n")
-		// test = append(test, username)
-		// test = append(test, newPost)
 	}
-	// fmt.Println(strconv.Itoa(id) + ": " + username + " " + newPost)
-	// fmt.Println(test)
-	// return test
 
 }
 
-func getInfo() []comment {
+func databaseComment(username string, newComment string) {
 
 	database, _ :=
-		sql.Open("sqlite3", "test.db")
+		sql.Open("sqlite3", "data.db")
+	statement, _ :=
+		database.Prepare("CREATE TABLE IF NOT EXISTS comment (id INTEGER PRIMARY KEY, username TEXT, newComment TEXT)")
+	statement.Exec()
+	statement, _ =
+		database.Prepare("INSERT INTO comment (username, newComment) VALUES (?, ?)")
+	fmt.Println("ici")
+	statement.Exec(username, newComment)
+	rows, _ :=
+		database.Query("SELECT id, username, newComment FROM comment")
+	var id int
+	var test []string
+	for rows.Next() {
+		rows.Scan(&id, &username, &newComment)
+		test = append(test, strconv.Itoa(id)+": "+username+" "+newComment+"\n")
+	}
+
+}
+
+func PostHandle(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Connect")
+
+	userName := r.FormValue("username")
+	newPost := r.FormValue("newPost")
+
+	if userName != "" && newPost != "" {
+		databasePost(userName, newPost)
+		fmt.Println("tu sors de  wsh")
+	}
+
+	tpl := template.Must(template.ParseFiles("assets/signIn.html"))
+
+	data := oui{
+		Poste: getPostInfo(),
+	}
+
+	err := tpl.Execute(w, data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func CommentHandle(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Connect")
+
+	userName := r.FormValue("username")
+	newComment := r.FormValue("newComment")
+
+	if userName != "" && newComment != "" {
+		databaseComment(userName, newComment)
+		fmt.Println("tu sors de  wsh")
+	}
+
+	tpl := template.Must(template.ParseFiles("assets/comment.html"))
+
+	data := oui{
+		Poste:   getPostInfo(),
+		Comment: getCommentInfo(),
+	}
+
+	err := tpl.Execute(w, data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func getPostInfo() []postMessage {
+
+	database, _ :=
+		sql.Open("sqlite3", "data.db")
 	rows, _ :=
 		database.Query("SELECT id, username, newPost FROM post")
 
 	var _id int
-	var test []comment
+	var test []postMessage
 	var _pseudo string
 	var _message string
 	for rows.Next() {
 		rows.Scan(&_id, &_pseudo, &_message)
-		data := comment{
+		data := postMessage{
 			id:      _id,
 			pseudo:  _pseudo,
 			message: _message,
@@ -168,62 +143,38 @@ func getInfo() []comment {
 		test = append(test, data)
 	}
 
-	// fmt.Println(strconv.Itoa(id) + ": " + username + " " + newPost)
-	// fmt.Println(test)
 	return test
 
 }
 
-func mainHandle(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("HOM")
-	// userName := r.FormValue("name")
-	// password := r.FormValue("password")
-	data := "test"
-	tpl := template.Must(template.ParseFiles("assets/index.html"))
+func getCommentInfo() []postMessage {
 
-	err := tpl.Execute(w, data)
-	if err != nil {
-		log.Fatal(err)
+	database, _ :=
+		sql.Open("sqlite3", "data.db")
+	rows, _ :=
+		database.Query("SELECT id, username, newComment FROM comment")
+
+	var _id int
+	var test []postMessage
+	var _pseudo string
+	var _message string
+	for rows.Next() {
+		rows.Scan(&_id, &_pseudo, &_message)
+		data := postMessage{
+			id:      _id,
+			pseudo:  _pseudo,
+			message: _message,
+		}
+		test = append(test, data)
 	}
-}
-
-func RegisterHandle(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Connect")
-
-	userName := r.FormValue("username")
-	newPost := r.FormValue("newPost")
-
-	if userName != "" && newPost != "" {
-		fmt.Println("tu rentre de dans wsh")
-		database(userName, newPost)
-		fmt.Println("tu sors de  wsh")
-		// http.Redirect(w, r, "/main", http.StatusSeeOther)
-	}
-
-	// var infoPost []string
-	// infoPost = append(infoPost, userName)
-	// infoPost = append(infoPost, newPost)
-	// println(&infoPost)
-
-	tpl := template.Must(template.ParseFiles("assets/test.html"))
-
-	data := oui{
-		Postee: getInfo(),
-	}
-
-	err := tpl.Execute(w, data)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	return test
 }
 
 func main() {
 	fs := http.FileServer(http.Dir("assets"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	http.Handle("/", fs)
-	http.HandleFunc("/main", mainHandle)
-
-	http.HandleFunc("/post", RegisterHandle)
-	http.ListenAndServe(":8082", nil)
+	http.HandleFunc("/account", PostHandle)
+	http.HandleFunc("/comment", CommentHandle)
+	http.ListenAndServe(":8080", nil)
 }
